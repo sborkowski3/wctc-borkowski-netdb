7f70ea47d9ea8e557fa9faa077d263bd27ec9dd9
Stephanie Borkowski <sborkowski3@my.wctc.edu>
2020-02-17 20:47:21 -0600

    Added getters and setters

--- SMBTicketConsole/Program.cs
+++ SMBTicketConsole/Program.cs
@@ -5,15 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace SMBTicketConsole
+namespace SMBTicketConsoleRevise
 {
+    class Ticket
+    {
+        public Uint64 TicketId { get; set; }
+        public string info { get; set; }
+        public string Display()
+        {
+            return $"id{TicketId}\n Info:{info}/n";
+
+        }
+    }
+
+
+    public class Uint64
+    {
+    }
+
     class Program
     {
+
+
         static void Main(string[] args)
         {
             String file = "TextFile1.txt";
             string line1 = "TicketID, Summary, Status, Priority, Submitter, Assigned, Watching";
             string choice;
+            //string count;
 
             Console.WriteLine("1) View an existing ticket");
             Console.WriteLine("2) Create a new ticket");
@@ -23,7 +42,7 @@ namespace SMBTicketConsole
             if (choice == "1")
             {
                 if (File.Exists(file))
-                {                   
+                {
                     StreamReader sr = new StreamReader(file);
                     while (!sr.EndOfStream)
                     {
@@ -36,25 +55,32 @@ namespace SMBTicketConsole
                 {
                     Console.WriteLine("File does not exist");
                 }
-                
-                if (choice == "2"){
-                int TicketID = 0;
-                    string line = sr.ReadLine();
-                    string[] arr = line.Split('|');
-                       Console.WriteLine("TicketID: {0}, Summary: {1}", arr[0], arr[1]);
-                       TicketID += arr[1] == " " ? 4 : arr[1] == " " ? 3;
-                    count += 1;
 
-               //string file = "TextFile.txt";
-               
-                }else
+                if (choice == "2")
+                {
+                    StreamWriter sw = new StreamWriter(file);
+                    sw.WriteLine(line1);
+                    sw.Close();
+
+                    //    int TicketID = 0;
+                    //    string line = Console.ReadLine();
+                    //    string[] arr = line.Split('|');
+                    //    Console.WriteLine("TicketID: {0}, Summary: {1}", arr[0], arr[1]);
+                    //TicketID += arr[0] == " " ? 4 : arr[1] == " " ? 3;
+                    //count += 1;
+
+                    //string file = "TextFile.txt";
+
+                }
+                else
                 {
                     Console.WriteLine("File does not exist");
                 }
-                
 
-            if (choice == 3){
-                Console.ReadLine();
+
+                if (choice == "3")
+                {
+                    Console.ReadLine();
                 }
             }
         }
