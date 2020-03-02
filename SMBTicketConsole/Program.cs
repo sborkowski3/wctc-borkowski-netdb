af392b91958d5f68b7c97802cf0bd2f9c778b739
sborkowski3 <60120757+sborkowski3@users.noreply.github.com>
2020-03-01 18:09:11 -0600

    Try to get new info on a new branch

--- SMBTicketConsole/Program.cs
+++ SMBTicketConsole/Program.cs
@@ -9,49 +9,50 @@ using NLog;
 
 private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
-namespace MovieTicketRedo
+namespace SMBTicketConsole
 {
- 
+
 
     static void Main(string[] args)
     {
-            logger.Info("Program started");
+        logger.Info("Program started");
 
-       string file = "../../data/movies.txt";
+        string file = "../../data/movies.txt";
 
-       
-            if (!File.Exists(file))
-            {
-                logger.Error("File does not exist: {File}", file);
 
- 
+        if (!File.Exists(file))
+        {
+            logger.Error("File does not exist: {File}", file);
+
+
 
             Console.WriteLine("1) View an existing Movie");
             Console.WriteLine("2) Create a new Movie");
             Console.WriteLine("3) Enter any key to exit");
-           choice = Console.ReadLine();
+            choice = Console.ReadLine();
 
-           if (choice == "1")
-           {
+            if (choice == "1")
+            {
                 if (File.Exists(movies.txt))
                 {
                     StreamReader sr = new StreamReader(file);
-                    for (int i = 0; i < MovieIds.Count; i++) {
-                    Console.WriteLine($"MovieID: {MovieIds[i]}");
-                      Console.WriteLine($"Title: {MovieTitles[i]}");
-                     Console.WriteLine($"Genre: {MovieGenres[i]}");
-                    Console.WriteLine($"Year: {MovieYears[i]}");
-                    Console.WriteLine();
+                    for (int i = 0; i < MovieIds.Count; i++)
+                    {
+                        Console.WriteLine($"MovieID: {MovieIds[i]}");
+                        Console.WriteLine($"Title: {MovieTitles[i]}");
+                        Console.WriteLine($"Genre: {MovieGenres[i]}");
+                        Console.WriteLine($"Year: {MovieYears[i]}");
+                        Console.WriteLine();
 
                     }
-                    
-while (!sr.EndOfStream)
+
+                    while (!sr.EndOfStream)
                     {
                         var line = sr.ReadLine();
                         Console.WriteLine(line);
-                       
-                  }
-                sr.Close();
+
+                    }
+                    sr.Close();
                 }
                 else
                 {
@@ -65,33 +66,33 @@ while (!sr.EndOfStream)
             {
                 MovieTicket = new MovieTicket();
 
-               // Console.Write("Enter Movie ID: ");
-              // var id = Console.ReadLine();
+                // Console.Write("Enter Movie ID: ");
+                // var id = Console.ReadLine();
 
-              //  Console.Write("Enter Movie Title: ");
-             //  string title = Console.ReadLine();
+                //  Console.Write("Enter Movie Title: ");
+                //  string title = Console.ReadLine();
 
-             //   Console.Write("Enter Movie Genre: ");
-             //  string gen = Console.ReadLine();
+                //   Console.Write("Enter Movie Genre: ");
+                //  string gen = Console.ReadLine();
 
-             //   Console.Write("Enter Movie Year: ");
-             //   String year = Console.ReadLine();
+                //   Console.Write("Enter Movie Year: ");
+                //   String year = Console.ReadLine();
 
-             //     StreamWriter sw = new StreamWriter(file, append:true);
-             //   sw.WriteLine(id);
-            //   sw.WriteLine(title);
-            //    sw.WriteLine(gen);
-            //   sw.WriteLine(year);
-     
-             //   sw.Close();   
+                //     StreamWriter sw = new StreamWriter(file, append:true);
+                //   sw.WriteLine(id);
+                //   sw.WriteLine(title);
+                //    sw.WriteLine(gen);
+                //   sw.WriteLine(year);
 
-           }
+                //   sw.Close();   
 
-           if (choice == "3")
-           {
-               Console.ReadLine();
-           }
-    logger.Info("Program ended");
+            }
+
+            if (choice == "3")
+            {
+                Console.ReadLine();
+            }
+            logger.Info("Program ended");
         }
     }
 }
